9fa40f58fac844ca2a176400a9ae046264564f0b
santhoshdhamotharan333-sudo <santhoshdhamotharan333@gmail.com>
2025-08-31 11:07:52 +0530

    Fix mixed content error by updating API URLs to HTTPS

--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,7 @@
 // src/api/products.js
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8000/api';
+const API_BASE = 'https://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_BASE,
